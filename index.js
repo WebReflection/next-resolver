0ca62f81b0f2fef838e7b6226edad61cdb3c2a24
webreflection <andrea.giammarchi@gmail.com>
2025-04-25 10:13:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+//@ts-check
+
+//@ts-ignore
+const withResolvers = Promise.withResolvers.bind(Promise);
+
+/**
+ * @template V
+ * @callback Resolve
+ * @param {V?} [value]
+ * @returns {void}
+ */
+
+/**
+ * @callback Reject
+ * @param {any?} [error]
+ * @returns {void}
+ */
+
+/**
+ * @template V
+ * @typedef {object} WithResolvers
+ * @prop {Promise<V>} promise
+ * @prop {Resolve<V>} resolve
+ * @prop {Reject} reject
+ */
+
+/**
+ * @template K,V
+ * @typedef {() => [K, Promise<V>]} Next
+ */
+
+/**
+ * @template K,V
+ * @callback Resolver
+ * @param {K} uid
+ * @param {V?} [value]
+ * @param {any?} [error]
+ */
+
+/**
+ * @template K,V
+ * @typedef {[Next<K,V>, Resolver<K,V>]} NextResolver
+ */
+
+/**
+ * @template K,V
+ * @param {(id: number) => K} [as]
+ * @returns
+ */
+export default (as = (id => /** @type {K} */(id))) => {
+  /** @type {Map<K,WithResolvers<V>>} */
+  const map = new Map;
+  let id = 0;
+  return /** @type {NextResolver<K,V>} */([
+    /** @type {Next<K,V>} */
+    () => {
+      let uid;
+      do { uid = as(id++) }
+      while (map.has(uid));
+      const wr = withResolvers();
+      map.set(uid, wr);
+      return [uid, wr.promise];
+    },
+    /** @type {Resolver<K,V>} */
+    (uid, value, error) => {
+      const wr = map.get(uid);
+      map.delete(uid);
+      if (error) wr?.reject(error);
+      else wr?.resolve(value);
+    },
+  ]);
+};
